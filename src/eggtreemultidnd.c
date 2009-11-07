ca25652fbf1b3e16305c97cb076fb3bb4c8bb2d9
Paolo Bacchilega <paobac@src.gnome.org>
2009-11-07 19:23:58 +0100

    use cp if the destination folder already exists
    we prefer mv instead of cp for performance reasons,
    but if the destination folder already exists mv
    doesn't work correctly.
    
    [bug #590027]

--- src/eggtreemultidnd.c
+++ src/eggtreemultidnd.c
@@ -309,6 +309,9 @@ egg_tree_multi_drag_motion_event (GtkWidget      *widget,
 
       selection = gtk_tree_view_get_selection (GTK_TREE_VIEW (widget));
       gtk_tree_selection_selected_foreach (selection, selection_foreach, &path_list);
+      if (path_list == NULL)
+	      return;
+
       path_list = g_list_reverse (path_list);
 
       model = gtk_tree_view_get_model (GTK_TREE_VIEW (widget));
