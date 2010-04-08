c52133b453f69d103ca708c9244096fe6ab26b72
Josselin Mouette <joss@malsain.org>
2010-04-08 10:12:55 +0200

    Uses dbus-glib but doesn't check for it
    dlg-package-installer.c unconditionally includes dbus/dbus-glib.h,
    but configure.ac only checks for dbus-glib-1 if packagekit support
    is requested. It should check for it always, to avoid build failures.
    
    [bug #615127]

--- src/dlg-package-installer.c
+++ src/dlg-package-installer.c
@@ -25,7 +25,11 @@
 #include <glib/gi18n.h>
 #include <gdk/gdkx.h>
 #include <gtk/gtk.h>
+
+#ifdef ENABLE_PACKAGEKIT
 #include <dbus/dbus-glib.h>
+#endif /* ENABLE_PACKAGEKIT */
+
 #include "dlg-package-installer.h"
 #include "gtk-utils.h"
 #include "main.h"
@@ -76,6 +80,7 @@ package_installer_terminated (InstallerData *idata,
 }
 
 
+#ifdef ENABLE_PACKAGEKIT
 static void
 packagekit_install_package_call_notify_cb (DBusGProxy     *proxy,
 					   DBusGProxyCall *call,
@@ -216,6 +221,7 @@ confirm_search_dialog_response_cb (GtkDialog *dialog,
 		installer_data_free (idata);
 	}
 }
+#endif /* ENABLE_PACKAGEKIT */
 
 
 void
@@ -226,8 +232,6 @@ dlg_package_installer (FrWindow  *window,
 	InstallerData   *idata;
 	GType            command_type;
 	FrCommand       *command;
-	DBusGConnection *connection;
-	gboolean         success = FALSE;
 
 	idata = g_new0 (InstallerData, 1);
 	idata->window = g_object_ref (window);
