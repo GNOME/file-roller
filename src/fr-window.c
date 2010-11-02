04b4a4e40d15fd97445f81849b04afcd13686c68
Paolo Bacchilega <paobac@src.gnome.org>
2010-11-02 13:28:15 +0100

    Specify the Internet site instead of the FSF address.

--- src/fr-window.c
+++ src/fr-window.c
@@ -15,8 +15,8 @@
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Street #330, Boston, MA 02111-1307, USA.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <config.h>
