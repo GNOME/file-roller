8120c29e27bd8a4604e4549010d301a1b6df9310
Paolo Bacchilega <paobac@src.gnome.org>
2012-08-10 12:16:06 +0200

    renamed batch-password.ui as ask-password.ui

--- src/dlg-ask-password.c
+++ src/dlg-ask-password.c
@@ -98,7 +98,7 @@ dlg_ask_password__common (FrWindow       *window,
 	char       *message;
 
 	data = g_new0 (DialogData, 1);
-	data->builder = _gtk_builder_new_from_resource ("batch-password.ui");
+	data->builder = _gtk_builder_new_from_resource ("ask-password.ui");
 	if (data->builder == NULL) {
 		g_free (data);
 		return;
