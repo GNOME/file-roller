ceb3232be494ef6841e368928a6a3e58029a0f97
Paolo Bacchilega <paobac@src.gnome.org>
2012-09-28 12:58:21 +0200

    try to open the archive again if the current command doesn't support reading
    this allows to have a command that can only write and another
    command that can only read an archive type.

--- src/fr-window.c
+++ src/fr-window.c
@@ -5941,6 +5941,12 @@ archive_list_ready_cb (GObject      *source_object,
 static void
 fr_window_archive_list (FrWindow *window)
 {
+	if (! fr_archive_is_capable_of (window->archive, FR_ARCHIVE_CAN_READ)) {
+		fr_window_archive_close (window);
+		fr_window_archive_open (window, window->priv->archive_file, NULL);
+		return;
+	}
+
 	_archive_operation_started (window, FR_ACTION_LISTING_CONTENT);
 	fr_archive_list (window->archive,
 			 window->priv->password,
