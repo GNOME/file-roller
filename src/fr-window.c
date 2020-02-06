5a7a468a1f31faa57fb888eb9259a174d93cd1b1
Sabri Ünal <libreajans@gmail.com>
2020-02-06 16:51:14 +0300

    window: Remove remnants of GtkToolbar code
    Fixes: https://gitlab.gnome.org/GNOME/file-roller/-/issues/69

--- src/fr-window.c
+++ src/fr-window.c
@@ -745,16 +745,6 @@ fr_window_update_paste_command_sensitivity (FrWindow     *window,
 	no_archive = (window->archive == NULL) || ! window->priv->archive_present;
 	ro         = ! no_archive && window->archive->read_only;
 
-#if 0
-	set_sensitive (window, "Paste",
-		       ! no_archive
-		       && ! ro
-		       && ! running
-		       && fr_archive_is_capable_of (window->archive, FR_ARCHIVE_CAN_STORE_MANY_FILES)
-		       && (window->priv->list_mode != FR_WINDOW_LIST_MODE_FLAT)
-		       && gtk_clipboard_wait_is_target_available (clipboard, FR_SPECIAL_URI_LIST));
-#endif
-
 	fr_window_enable_action (window, "edit-paste",
 				 ! no_archive
 				 && ! ro
@@ -1120,49 +1110,12 @@ fr_window_update_sensitivity (FrWindow *window)
 	one_file_selected    = n_selected == 1;
 	dir_selected         = selection_has_a_dir (window);
 
-#if 0
-	set_sensitive (window, "Add", ! no_archive && ! ro && ! running && can_store_many_files);
-	set_sensitive (window, "Add_Toolbar", ! no_archive && ! ro && ! running && can_store_many_files);
-	set_sensitive (window, "Copy", ! no_archive && ! ro && ! running && can_store_many_files && sel_not_null && (window->priv->list_mode != FR_WINDOW_LIST_MODE_FLAT));
-	set_sensitive (window, "Cut", ! no_archive && ! ro && ! running && can_store_many_files && sel_not_null && (window->priv->list_mode != FR_WINDOW_LIST_MODE_FLAT));
-	set_sensitive (window, "Delete", ! no_archive && ! ro && ! window->priv->archive_new && ! running && can_store_many_files);
-	set_sensitive (window, "DeselectAll", ! no_archive && sel_not_null);
-	set_sensitive (window, "Extract", file_op);
-	set_sensitive (window, "Extract_Toolbar", file_op);
-	set_sensitive (window, "Find", ! no_archive);
-	set_sensitive (window, "New", ! running);
-	set_sensitive (window, "Open", ! running);
-	set_sensitive (window, "Open_Toolbar", ! running);
-	set_sensitive (window, "OpenSelection", file_op && sel_not_null && ! dir_selected);
-	set_sensitive (window, "OpenFolder", file_op && one_file_selected && dir_selected);
-	set_sensitive (window, "Password", ! running && (! no_archive && window->archive->propPassword));
-	set_sensitive (window, "Properties", file_op);
-	set_sensitive (window, "Close", !running || window->priv->stoppable);
-	set_sensitive (window, "Reload", ! (no_archive || running));
-	set_sensitive (window, "Rename", ! no_archive && ! ro && ! running && can_store_many_files && one_file_selected);
-	set_sensitive (window, "SaveAs", ! no_archive && can_store_many_files && ! running);
-	set_sensitive (window, "SelectAll", ! no_archive);
-	set_sensitive (window, "TestArchive", ! no_archive && ! running && window->archive->propTest);
-	set_sensitive (window, "ViewSelection", file_op && one_file_selected && ! dir_selected);
-	set_sensitive (window, "ViewSelection_Toolbar", file_op && one_file_selected && ! dir_selected);
-#endif
-
 	if (window->priv->progress_dialog != NULL)
 		gtk_dialog_set_response_sensitive (GTK_DIALOG (window->priv->progress_dialog),
 						   GTK_RESPONSE_OK,
 						   running && window->priv->stoppable);
 
 	fr_window_update_paste_command_sensitivity (window, NULL);
-
-#if 0
-	set_sensitive (window, "SelectAll", (window->priv->current_view_length > 0) && (window->priv->current_view_length != n_selected));
-	set_sensitive (window, "DeselectAll", n_selected > 0);
-
-	set_sensitive (window, "ViewSidebar", (window->priv->list_mode == FR_WINDOW_LIST_MODE_AS_DIR));
-
-	set_sensitive (window, "ViewAllFiles", ! window->priv->filter_mode);
-	set_sensitive (window, "ViewAsFolder", ! window->priv->filter_mode);
-#endif
 
 	fr_window_enable_action (window, "add-files", ! no_archive && ! ro && ! running && can_store_many_files);
 	fr_window_enable_action (window, "close", ! running || window->priv->stoppable);
