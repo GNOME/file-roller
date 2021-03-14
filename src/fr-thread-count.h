d9bf793c50e71372c112d6fbe547640bc7330d87
Julian Groß <firedefender1@googlemail.com>
2021-03-14 20:03:34 +0100

    Add function to define thread count

---
+++ src/fr-thread-count.h
@@ -0,0 +1,29 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+
+/*
+ *  File-Roller
+ *
+ *  Copyright (C) 2021 Free Software Foundation, Inc.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef FR_THREAD_COUNT_H
+#define FR_THREAD_COUNT_H
+
+#include <glib.h>
+
+gchar * fr_get_thread_count (void);
+
+#endif /* FR_THREAD_COUNT_H */
