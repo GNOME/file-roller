79b60ce94c55bf1bd28f355eea5f9205b58923f6
Paolo Bacchilega <paobac@src.gnome.org>
2024-05-08 09:12:45 +0200

    Revert "meson: automatically update releases in appdata"
    This reverts commit d90e49cc0c115064e78d6c7a98e8d57dbaa34731.

--- data/metainfo-releases-tarball.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-appstreamcli news-to-metainfo --limit=3 $MESON_DIST_ROOT/NEWS $MESON_DIST_ROOT/data/org.gnome.FileRoller.appdata.xml.in $MESON_DIST_ROOT/data/org.gnome.FileRoller.appdata.xml.in
